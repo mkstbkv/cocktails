72a6d383d2a410f1db8c790789737e7003dd57fa
Tugolbai Maksatbek uulu <mkstbkv751@gmail.com>
2022-04-09 02:03:13 +0600

    вывод одного коктейля

--- frontend/src/app/pages/cocktail-details/cocktail-details.component.ts
+++ frontend/src/app/pages/cocktail-details/cocktail-details.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/types';
+import { ActivatedRoute } from '@angular/router';
+import { fetchCocktailRequest } from '../../store/cocktails/cocktails.actions';
+import { Cocktail } from '../../models/cocktail.model';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cocktail-details',
   templateUrl: './cocktail-details.component.html',
   styleUrls: ['./cocktail-details.component.sass']
 })
-export class CocktailDetailsComponent implements OnInit {
+export class CocktailDetailsComponent implements OnInit, OnDestroy {
+  cocktail: Observable<Cocktail | null>
+  cocktailOne!: Cocktail;
+  loading: Observable<boolean>
+  error: Observable<null | string>
+  cockSub!: Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store<AppState>, private route: ActivatedRoute) {
+    this.cocktail = store.select(state => state.cocktails.cocktail);
+    this.loading = store.select(state => state.cocktails.fetchLoading);
+    this.error = store.select(state => state.cocktails.fetchError);
   }
 
+  ngOnInit()  {
+    this.store.dispatch(fetchCocktailRequest({id: this.route.snapshot.params['id']}));
+    this.cockSub = this.cocktail.subscribe(cocktail => {
+      if (cocktail) {
+        this.cocktailOne = cocktail;
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.cockSub.unsubscribe()
+  }
 }

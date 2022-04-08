61bb4594131c224b8847263eb620d3a860dede96
Tugolbai Maksatbek uulu <mkstbkv751@gmail.com>
2022-04-08 15:45:06 +0600

    сущность коктейля

---
+++ api/models/Cocktail.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const IngredientSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    }
+})
+
+const CocktailSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    recipe: {
+        type: String,
+        required: true
+    },
+    is_published: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    ingredients: [
+        IngredientSchema
+    ],
+});
+
+const Cocktail = mongoose.model('Cocktail', CocktailSchema);
+
+module.exports = Cocktail;
